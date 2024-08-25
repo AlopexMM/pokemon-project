d3edc4d0865698b030bc3c1d2bb9a0520b91b49b
mario <mariomori00@gmail.com>
2024-08-25 15:12:51 -0300

    Added a script to setup the environment

---
+++ check-container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pokemonContainer=`docker ps | grep pokemon-creator`
+
+if [ $pokemonContainer != ""] then
+docker stop pokemon-creator
+docker rm pokemon-creator
+docker rmi $(docker images | grep pokemon-creator | awk '{print $3}')
+fi
