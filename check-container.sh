b10c59ec0a76b514b0c355f73c0862d7e34504d6
mario <mariomori00@gmail.com>
2024-08-25 15:17:51 -0300

    fix check container

--- check-container.sh
+++ check-container.sh
@@ -2,8 +2,8 @@
 
 pokemonContainer=`docker ps | grep pokemon-creator`
 
-if [ $pokemonContainer != ""] then
-docker stop pokemon-creator
-docker rm pokemon-creator
-docker rmi $(docker images | grep pokemon-creator | awk '{print $3}')
+if [[ $pokemonContainer != "" ]] then
+    docker stop pokemon-creator
+    docker rm pokemon-creator
+    docker rmi $(docker images | grep pokemon-creator | awk '{print $3}')
 fi
