39e786b1964b5b39bff53023e5361e53e660b7ac
Mario Mori <mario_mori@hotmail.com>
2024-08-26 11:30:51 -0300

    Add printing on sh script

--- check-container.sh
+++ check-container.sh
@@ -5,6 +5,8 @@ then
     docker stop pokemon-creator
     docker rm pokemon-creator
     docker rmi $(docker images | grep pokemon-creator | awk '{print $3}')
+    echo "Deleted container and image"
 else
+echo "Nothing to delete"
 exit 0
 fi
